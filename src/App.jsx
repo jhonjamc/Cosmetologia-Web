30aeaa14f3eb65f4a81e1cda98db03b5a862fb47
Jhon Jairo Moguea <jhonjairomoguea630@gmail.com>
2026-06-13 16:49:06 -0500

    Cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Productos from './components/Productos';
 
 function App() {
   return (
-    <Router>
+    <Router >
       <Routes>
         {/* Ruta principal con toda tu estructura */}
         <Route
